eaf21c9b243a6350d773d9712637d7599c797e55
ritikupadhyay <ritikupadhyay123@gmail.com>
2024-09-20 00:59:31 +0530

    dynamic routing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express =require("express")
 
 const app=express()
 
+app.use("/",(req,res)=>{
+    res.send("hello");
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000.....")
 });
