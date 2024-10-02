7ea52437204eac52d04a8a011673a10ec51a8c30
ritikupadhyay <ritikupadhyay123@gmail.com>
2024-10-02 20:06:49 +0530

    sending connection request

--- src/models/user.js
+++ src/models/user.js
@@ -39,11 +39,15 @@ const userSchema = new mongoose.Schema(
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender dataa is not valid");
-        }
+      enum:{
+        values:["male","female","others"],
+        message:`{VALUE} is not a valid gender type`,
       },
+      // validate(value) {
+      //   if (!["male", "female", "others"].includes(value)) {
+      //     throw new Error("Gender dataa is not valid");
+      //   }
+      // },
     },
     photoUrl: {
       type: String,
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -47,4 +47,11 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout",async(req,res)=>{
+  res.cookie("token",null,{
+    expires:new Date(Date.now())
+  });
+  res.send("Logged OUt");
+})
+
 module.exports = authRouter;
